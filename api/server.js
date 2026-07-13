4017cf025f3f739efe689dd0de42dcb4ec8490b6
Cole McMannus <cmcmannus@juliusworks.com>
2026-07-13 03:13:21 -0400

    refactor: rename users endpoint to members

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // API routes
-app.use('/api/users', usersRouter);
+app.use('/api/members', usersRouter);
 app.use('/api/tasks', tasksRouter);
 app.use('/api/health', healthRouter);
 
